8830dbf1be1c4aef818516662a93e5daccf71935
Sara Garzou <sgarzou@proton.me>
2025-12-02 22:39:05 +0100

    Add solutions day 2

---
+++ day2/day2-part1.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import Data.List (foldl')
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn delimiter str = case break (== delimiter) str of
+    (before, []) -> [before]
+    (before, _:after) -> before : splitOn delimiter after
+
+main :: IO ()
+main = do
+    input <- getContents
+    let line = head (lines input)
+    let split = splitOn ',' line
+    let numbers = concat [ [start..end] | (start, end) <- map parseRange split ]
+    let total = foldl' (+) 0 (filter isInvalid numbers)
+    putStrLn ("Sum of invalid IDs: " ++ show total)
+
+parseRange :: String -> (Int, Int)
+parseRange str = (from, to)
+    where
+        dashSplit = splitOn '-' str
+        from = read (head dashSplit) :: Int
+        to = read (dashSplit !! 1) :: Int
+
+isInvalid :: Int -> Bool
+isInvalid number = even len && firstHalf == secondHalf
+    where
+        s = show number
+        len = length s
+        (firstHalf, secondHalf) = splitAt (len `div` 2) s
